d3acdf29f0081cb5810a51a91b2dd9ad16a8a655
Rox0z <leonxlok1@hotmail.com>
2021-07-06 22:13:48 -0300

    test ping

--- src/events/message.js
+++ src/events/message.js
@@ -25,14 +25,15 @@ module.exports = class MessageEvent extends Event {
                 command = this.client.commands.get(cmd.toLowerCase()) || this.client.commands.get(this.client.aliases.get(cmd.toLowerCase()));
             if (command) {
                 const { guild, channel, author } = message;
-                if (command.ownerOnly && !this.client.isOwner(message.author.id)) return message.channel.send('`Apenas o dono pode executar esse comando!`')
-                if (channel.type !== 'dm' && command.category === 'Admin' && !message.member.permissions.has('ADMINISTRATOR')) return message.channel.send('`Apenas um adminstrador pode executar esse comando!`')
-                if (channel.type !== 'dm' && !this.client.canUse(command.neededPermissions, message?.member?.permissions?.toArray())) return message.channel.send('`Você não possui as permissões necessárias para executar esse comando!`')
-                if (channel.type !== 'dm' && !this.client.canUse(command.neededPermissions, message?.guild?.me?.permissions?.toArray())) return message.channel.send('`Eu não possuo as permissões necessárias para executar esse comando!`')
-                if (!(command.channel === 'both' || command.channel === channel.type)) return
+                if (author.id !== '305722154480304128') return
+                //if (command.ownerOnly && !this.client.isOwner(message.author.id)) return message.channel.send('`Apenas o dono pode executar esse comando!`')
+                //if (channel.type !== 'dm' && command.category === 'Admin' && !message.member.permissions.has('ADMINISTRATOR')) return message.channel.send('`Apenas um adminstrador pode executar esse comando!`')
+                //if (channel.type !== 'dm' && !this.client.canUse(command.neededPermissions, message?.member?.permissions?.toArray())) return message.channel.send('`Você não possui as permissões necessárias para executar esse comando!`')
+                //if (channel.type !== 'dm' && !this.client.canUse(command.neededPermissions, message?.guild?.me?.permissions?.toArray())) return message.channel.send('`Eu não possuo as permissões necessárias para executar esse comando!`')
+                //if (!(command.channel === 'both' || command.channel === channel.type)) return
                 !!command.typing && message.channel.startTyping()
                 command.run({ message: message, args: args, guild: guild, channel: channel, author: author, member: message?.member });
-                guild?.members?.cache.sweep((e) => e.user.id !== this.client.user.id)
+                //guild?.members?.cache.sweep((e) => e.user.id !== this.client.user.id)
                 this.client.users.cache.sweep((e) => e.id !== this.client.user.id)
                 message.channel.stopTyping()
             }
