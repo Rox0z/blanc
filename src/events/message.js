ca1d695ac41c3241358b42e1bebfc7d27614ece2
Rox0z <leonxlok1@hotmail.com>
2021-07-08 18:08:09 -0300

    new locale

--- src/events/message.js
+++ src/events/message.js
@@ -20,7 +20,7 @@ module.exports = class MessageEvent extends Event {
             message.content.match(mention) && message.reply(this.client.locale(lang, 'GUILD_PREFIX', {custom: ['prefix', dbprefix]}));
             prefix = message.content.match(mentionPrefix) ? message.content.match(mentionPrefix)[0] : (this.client.defaultPrefix !== dbprefix) ? dbprefix : this.client.defaultPrefix;
         } else {
-            message.content.match(mention) && message.reply(this.client.text(lang, 'BOT_PREFIX', {custom: ['prefix', this.client.defaultPrefix]}))
+            message.content.match(mention) && message.reply(this.client.locale(lang, 'BOT_PREFIX', {custom: ['prefix', this.client.defaultPrefix]}))
             prefix = message.content.match(mentionPrefix) ? message.content.match(mentionPrefix)[0] : this.client.defaultPrefix
         }
         if (message.content.startsWith(prefix)) {
@@ -28,13 +28,13 @@ module.exports = class MessageEvent extends Event {
                 command = this.client.commands.get(cmd.toLowerCase()) || this.client.commands.get(this.client.aliases.get(cmd.toLowerCase()));
             if (command) {
                 const { guild, channel, author } = message;
-                if (command.ownerOnly && !this.client.isOwner(message.author.id)) return message.channel.send(this.client.text(lang, 'OWNER_ONLY_ERROR'))
-                if (channel.type !== 'dm' && command.category === 'Admin' && !message.member.permissions.has('ADMINISTRATOR')) return message.channel.send(this.client.text(lang, 'ADMIN_ONLY_ERROR'))
-                if (channel.type !== 'dm' && !this.client.canUse(command.neededPermissions, message?.member?.permissions?.toArray())) return message.channel.send(this.client.text(lang, 'USER_PERM_ERROR'))
-                if (channel.type !== 'dm' && !this.client.canUse(command.neededPermissions, message?.guild?.me?.permissions?.toArray())) return message.channel.send(this.client.text(lang, 'CLIENT_PERM_ERROR'))
+                if (command.ownerOnly && !this.client.isOwner(message.author.id)) return message.channel.send(this.client.locale(lang, 'ERROR_OWNER_ONLY'))
+                if (channel.type !== 'dm' && command.category === 'Admin' && !message.member.permissions.has('ADMINISTRATOR')) return message.channel.send(this.client.locale(lang, 'ERROR_ADMIN_ONLY'))
+                if (channel.type !== 'dm' && !this.client.canUse(command.neededPermissions, message?.member?.permissions?.toArray())) return message.channel.send(this.client.locale(lang, 'ERROR_USER_PERM'))
+                if (channel.type !== 'dm' && !this.client.canUse(command.neededPermissions, message?.guild?.me?.permissions?.toArray())) return message.channel.send(this.client.locale(lang, 'ERROR_CLIENT_PERM'))
                 if (!(command.channel === 'both' || command.channel === channel.type)) return
                 !!command.typing && message.channel.startTyping()
-                command.run({ message: message, args: args, guild: guild, channel: channel, author: author, member: message?.member, prefix: prefix });
+                command.run({ message, args, guild, channel, author, member: message?.member, prefix, lang });
                 guild?.members?.cache.sweep((e) => e.user.id !== this.client.user.id)
                 this.client.users.cache.sweep((e) => e.id !== this.client.user.id)
                 message.channel.stopTyping()
