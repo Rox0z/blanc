150f7ab875b5013f21253f2f969809e88802cbad
Leonardo Marinho <61660507+Rox0z@users.noreply.github.com>
2021-07-09 11:08:24 -0300

    Update steamscam.js
    v13 update

--- src/events/steamscam.js
+++ src/events/steamscam.js
@@ -10,7 +10,7 @@ module.exports = class ScamEvent extends Event {
         })
     }
     async run(message) {
-        if (message.content.match(/[(http(s)?):\/\/(www\.)?\w]{2,256}\.ru\//gim)) {
+        if (message?.content?.match(/[(http(s)?):\/\/(www\.)?\w]{2,256}\.ru\//gim)) {
             let content = message.content
             const banned = await message.guild.members
             .ban(message.author.id, { days: 1, reason: `Scam link with .ru` })
@@ -18,9 +18,9 @@ module.exports = class ScamEvent extends Event {
             let channel = await this.client.channels.fetch('722147859184746656')
             const embed = new MessageEmbed()
                 .setTitle(`BANNED - Steam Scam from: ${message.author.username} (${message.author.id})`)
-                .setDescription(content)
+                .setDescription('```'+content+'```')
                 .setColor('#f00')
-            message.delete().then(channel.send(embed))
+            message.delete().then(channel.send({embeds: [embed]}))
         } 
     }
 }
