de231f1108f0924cfd5f496c49c3461a64ce165d
Rox0z <leonxlok1@hotmail.com>
2021-07-13 11:57:43 -0300

    dumb

--- src/commands/casual/fakeprofile.js
+++ src/commands/casual/fakeprofile.js
@@ -11,7 +11,7 @@ module.exports = class AvatarCommand extends Command {
         })
     }
     async run({ message, args }) {
-        args.join('  ').replace(/  branco/gi, '  white').split('  ')
+        args = args.join('  ').replace(/  branco/gi, '  white').split('  ')
         message.nmReply({ files: [this.client.utils.attach(await this.client.gen.fakeProfile(message, args[0], args[1], args[2]), 'profile.png')] });
     }
 }
