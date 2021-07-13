abd8bc0aec78be3e72c317ac3c111fd82c40a347
Rox0z <leonxlok1@hotmail.com>
2021-07-13 11:54:04 -0300

    branco

--- src/commands/casual/fakeprofile.js
+++ src/commands/casual/fakeprofile.js
@@ -6,11 +6,12 @@ module.exports = class AvatarCommand extends Command {
             aliases: ['fake'],
             description: { pt: "Gera um card de perfil.", en: "Generate a profile card." },
             category: 'Casual',
-            usage: { pt: "fake [memberoID|@membero] [bannerURL|anexo] [white?]", en: "fake [memberID|@member] [bannerURL|attach] [white?]" },
+            usage: { pt: "fake [memberoID|@membero] [bannerURL|anexo] [branco?]", en: "fake [memberID|@member] [bannerURL|attach] [white?]" },
             title: { pt: "Perfil fake", en: "Fake profile" }
         })
     }
     async run({ message, args }) {
+        args.join('  ').replace(/  branco/gi, '  white').split('  ')
         message.nmReply({ files: [this.client.utils.attach(await this.client.gen.fakeProfile(message, args[0], args[1], args[2]), 'profile.png')] });
     }
 }
