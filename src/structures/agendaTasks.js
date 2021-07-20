85a466b11605595ba5134743c57367c65cbc91d8
Rox0z <leonxlok1@hotmail.com>
2021-07-20 17:44:35 -0300

    auto unmute log

--- src/structures/agendaTasks.js
+++ src/structures/agendaTasks.js
@@ -5,22 +5,34 @@ module.exports = class tasks {
         agenda.define('unmute',
             async (job) => {
                 const { memberID, guildID, muteroleID } = job.attrs.data;
-                //console.table({ memberID, guildID, muteroleID })
                 let guild = this.client.utils.resolveGuild(guildID, this.client.guilds.cache)
                 let member = await guild.members.fetch(memberID)
                 member.roles.remove([muteroleID])
+                let ch = await this.client.guildConfig.get(`${guildID}.modLogsChannel`)
+                if (typeof ch === 'string') {
+                    let reason = {
+                        en: 'It\'s time to speak again!',
+                        pt: 'É hora de falar de novo!',
+                    }
+                    let lang;
+                    if (this.client.locales.get(guildID) === null) lang = guild.preferredLocale.split('-')[0] === 'pt' ? 'pt' : 'en'
+                    else lang = this.client.locales.get(guildID)
+                    let logchannel = await this.client.utils.resolveChannel(guild, ch)
+                    if (!logchannel) return await this.client.guildConfig.set(`${guild.id}.modLogsChannel`, null).catch(() => null)
+                    logchannel.send({ embeds: [this.client.embedder.modLog(guild, this.client.user, member.user, reason[lang], 'UNMUTE', lang)] }).catch(() => null)
+                }
                 guild?.members?.cache.sweep((e) => e.user.id !== this.client.user.id)
                 await job.remove()
             })
-                agenda.define('temprole',
-                async (job) => {
+        agenda.define('temprole',
+            async (job) => {
                 const { memberID, guildID, rolesID } = job.attrs.data;
                 let guild = this.client.utils.resolveGuild(guildID, this.client.guilds.cache)
-                    let member = await guild.members.fetch(memberID)
-                    member.roles.remove(rolesID)
-                    guild?.members?.cache.sweep((e) => e.user.id !== this.client.user.id)
-                    await job.remove()
-                })
+                let member = await guild.members.fetch(memberID)
+                member.roles.remove(rolesID)
+                guild?.members?.cache.sweep((e) => e.user.id !== this.client.user.id)
+                await job.remove()
+            })
     }
 
 }
