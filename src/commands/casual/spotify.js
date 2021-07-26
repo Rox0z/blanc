738b4acd2b7b700ff56bf40d31bf2a7484df079f
Rox0z <leonxlok1@hotmail.com>
2021-07-26 18:39:47 -0300

    spotify command

---
+++ src/commands/casual/spotify.js
@@ -0,0 +1,40 @@
+const Command = require('../../structures/command.js'),
+    { MessageEmbed } = require('discord.js-light');
+
+module.exports = class NameCommand extends Command {
+    constructor(...args) {
+        super(...args, {
+            aliases: ['spot', 'music'],
+            description: { pt: 'Mostra um cover da musica tocada no Spotify do usuário.', en: 'Shows a cover image from user current Spotify music.' },
+            category: 'Casual',
+            channel: 'text',
+            title: 'Spotify',
+            usage: { pt: 'spotify [usuárioID|@usuário]', en: 'spotify [userID|@user]' },
+        })
+    }
+    async run({ message, args, guild, channel, author, prefix, lang }) {
+        const user = await this.client.utils.resolveUser(message, args[0])
+        let spot = 'Not found'
+        user.presence.activities.find((act) => {
+            if (
+                act.type == "LISTENING" &&
+                act.name == "Spotify" &&
+                act.assets !== null
+            ) {
+
+                spot = act;
+            }
+        });
+        if (spot === 'Not found') {
+            message.nmReply(this.client.locale(lang, 'ERROR_NOT_SPOTIFY'))
+        } else {
+            let image = await this.client.gen.spotifyCover(spot);
+            const attachment = this.client.utils.attach(image, "spotify.png");
+            const spotify = new MessageEmbed()
+                .setColor("#1db954")
+                .setImage("attachment://spotify.png");
+
+            message.nmReply({ embeds: [spotify], files: [attachment] })
+        }
+    }
+}
