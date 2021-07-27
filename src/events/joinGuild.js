8b30ed3310beed61b66ad54b5dd522f4d1aa48c4
Rox0z <leonxlok1@hotmail.com>
2021-07-27 01:43:19 -0300

    add locale to cache when join

--- src/events/joinGuild.js
+++ src/events/joinGuild.js
@@ -11,6 +11,7 @@ module.exports = class JoinEvent extends Event {
     async run(guild) {
         this.client.guildConfig.set(`${guild.id}`, {muteRole: null, logsChannel: null, modLogsChannel: null, guildPrefix: this.client.defaultPrefix, guildLocale: guild.preferredLocale.split('-')[0] === 'pt' ? 'pt' : 'en'})
         this.client.prefixes.set(guild.id, this.client.defaultPrefix)
+        this.client.locales.set(guild.id, guild.preferredLocale.split('-')[0] === 'pt' ? 'pt' : 'en')
         console.log(`Novo servidor: ${guild.name}`)
     }
 }
