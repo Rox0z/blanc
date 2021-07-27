0271b7fe4e0b2260cd493a0ea8714f951b811178
Rox0z <leonxlok1@hotmail.com>
2021-07-26 21:03:55 -0300

    remove then

--- src/events/joinGuild.js
+++ src/events/joinGuild.js
@@ -11,7 +11,7 @@ module.exports = class JoinEvent extends Event {
     async run(guild) {
         this.client.guildConfig.set(`${guild.id}`, {muteRole: null, logsChannel: null, modLogsChannel: null, guildPrefix: this.client.defaultPrefix, guildLocale: guild.preferredLocale.split('-')[0] === 'pt' ? 'pt' : 'en'})
         this.client.prefixes.set(guild.id, this.client.defaultPrefix)
-        .then(()=>console.log(`Novo servidor: ${guild.name}`))
+        console.log(`Novo servidor: ${guild.name}`)
         this.client.users.cache.sweep((e) => e.id !== this.client.user.id)
     }
 }
