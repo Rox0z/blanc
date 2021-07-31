e97bb2ddbc9818314700e0fb9f39dfa2eb0933e0
Rox0z <leonxlok1@hotmail.com>
2021-07-31 20:05:25 -0300

    mass ban

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,4 +1,5 @@
-const Command = require("../../structures/command.js");
+const Command = require("../../structures/command.js"),
+    { MessageEmbed } = require('discord.js');
 
 module.exports = class BanCommand extends Command {
     constructor(...args) {
@@ -14,20 +15,33 @@ module.exports = class BanCommand extends Command {
     }
     async run({ args, message, guild, author, channel, lang }) {
         if (!args[0]) return message.nmReply(this.client.locale(lang, 'ERROR_NO_USER'))
-        let user = await this.client.utils.resolveUser(message, args[0], {author:false, mention: false})
-        if (!user) return message.nmReply(this.client.locale(lang, 'ERROR_INVALID_USER'))
-        if (user.id === message.author.id) return message.channel.send(this.client.locale(lang, 'ERROR_SELFPUNISH'))
-
-        let reason = args.slice(1).join(' ')
-        await guild.members
-            .ban(user.id, { reason: reason })
-            .then(message.nmReply(this.client.locale(lang, 'USER_PUNISHED')));
-
+        let { users, reason, fails } = await this.client.utils.multiResolver(message, args)
+        if (!users) return message.nmReply(this.client.locale(lang, 'ERROR_INVALID_USER'))
+        if (users.includes(author)) return message.channel.send(this.client.locale(lang, 'ERROR_SELFPUNISH'))
+        let banned = new Array()
+        for (let [i, user] of users.entries()) {
+            let bwned = await guild.bans.fetch(user.id).catch(() => null), ban
+            if (!bwned) { ban = await guild.members.ban(user.id, { reason }).catch(() => null) }
+            bwned ? (delete users[i] && banned.push(`${user.id} (${user.tag})`)) : ban ? null : (delete users[i] && fails.push(`${user.id} (${user.tag})`))
+        }
+        users = users.filter(u => u)
+        message.react('841742417303961622')
+        let color
+        if (fails.length >= 1 && banned.length >= 1) {color = '#ff0000'} else if (fails.length >= 1 || banned.length >= 1) {color = '#ff8800'} else {color = '#00ff00'}
+        const embed = new MessageEmbed()
+            .setColor(color)
+            .setTitle(this.client.locale(lang, 'USER_PUNISHED'))
+            .addField(this.client.locale(lang, 'REASON'), `\`\`\`${reason.length === 0 ? this.client.locale(lang, 'NO_REASON') : reason}\`\`\``, false)
+        if (banned.length >= 1) { embed.addField(this.client.locale(lang, 'ALREADY_BANNED'), `\`\`\`\n${banned.join('\n')}\`\`\``, false) }
+        if (fails.length >= 1) { embed.addField(this.client.locale(lang, 'ERROR_FAILED_BANS'), `\`\`\`\n${fails.join('\n')}\`\`\``, false) }
+        message.nmReply({embeds: [embed]})
         let ch = await this.client.guildConfig.get(`${guild.id}.modLogsChannel`)
         if (typeof ch === 'string') {
             let logchannel = await this.client.utils.resolveChannel(guild, ch)
             if (!logchannel) return await this.client.guildConfig.set(`${guild.id}.modLogsChannel`, null).catch(() => null)
-            logchannel.send({ embeds: [this.client.embedder.modLog(guild, author, user, reason.length === 0 ? this.client.locale(lang, 'NO_REASON') : reason, 'BAN', lang)] }).catch(() => channel.send(this.client.locale(lang, 'ERROR_CANNOT_LOG')))
+            for (let user of users) {
+                logchannel.send({ embeds: [this.client.embedder.modLog(guild, author, user, reason.length === 0 ? this.client.locale(lang, 'NO_REASON') : reason, 'BAN', lang)] }).catch(() => channel.send(this.client.locale(lang, 'ERROR_CANNOT_LOG')))
+            }
         }
     }
 };
